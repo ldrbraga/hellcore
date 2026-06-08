b81210935c22634931073bdcb3355c3dfead8212
Leandro Braga <ldrbraga@gmail.com>
2026-06-08 09:58:58 -0300

    perf: reduce ISR revalidate to 60s for near-instant sheet updates

--- app/lib/sheets.ts
+++ app/lib/sheets.ts
@@ -47,7 +47,7 @@ export async function getProducts(): Promise<Product[]> {
     `https://sheets.googleapis.com/v4/spreadsheets/${process.env.GOOGLE_SHEETS_ID}/values/${range}`,
     {
       headers: { Authorization: `Bearer ${token}` },
-      next: { revalidate: 3600 },
+      next: { revalidate: 60 },
     }
   );
 
